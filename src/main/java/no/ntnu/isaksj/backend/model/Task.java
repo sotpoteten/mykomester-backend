0b856592b844fb635385dd1da2354c73bf66a9da
Isak Jonsson <isaksj@stud.ntnu.no>
2025-02-24 21:44:17 +0100

    Lagt til getters og setters i Task

--- src/main/java/no/ntnu/isaksj/backend/model/Task.java
+++ src/main/java/no/ntnu/isaksj/backend/model/Task.java
@@ -35,6 +35,59 @@ public class Task {
     @JsonBackReference(value = "species-task")
     private Species species;
 
+    public Long getId() {
+        return id;
+    }
 
-    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getAnsweredSpecies() {
+        return answeredSpecies;
+    }
+
+    public void setAnsweredSpecies(String answeredSpecies) {
+        this.answeredSpecies = answeredSpecies;
+    }
+
+    public Normliststatus getAnsweredCategory() {
+        return answeredCategory;
+    }
+
+    public void setAnsweredCategory(Normliststatus answeredCategory) {
+        this.answeredCategory = answeredCategory;
+    }
+
+    public String getAnsweredNote() {
+        return answeredNote;
+    }
+
+    public void setAnsweredNote(String answeredNote) {
+        this.answeredNote = answeredNote;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
 }
