efbdb0c799b90f7949ad9828291459801436bd81
Isak Jonsson <isaksj@stud.ntnu.no>
2025-03-08 17:41:24 +0100

    Funksjon for å sette standardinnstillinger på opprettelse av ny bruker

--- src/main/java/no/ntnu/isaksj/backend/service/UserSettingsService.java
+++ src/main/java/no/ntnu/isaksj/backend/service/UserSettingsService.java
@@ -4,6 +4,9 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import no.ntnu.isaksj.backend.enums.AnswerMode;
+import no.ntnu.isaksj.backend.enums.QuizContent;
+import no.ntnu.isaksj.backend.enums.QuizMode;
 import no.ntnu.isaksj.backend.model.User;
 import no.ntnu.isaksj.backend.model.UserSettings;
 import no.ntnu.isaksj.backend.repository.UserSettingsRepository;
@@ -25,5 +28,13 @@ public class UserSettingsService {
 
     public UserSettings findByUser(User user) {
         return userSettingsRepository.findByUser(user);
+    }
+
+    public UserSettings setStandardSettings(UserSettings settings) {
+        settings.setNrOfTasks(30);
+        settings.setQuizContent(QuizContent.HELE_PENSUM);
+        settings.setQuizMode(QuizMode.STANDARD);
+        settings.setAnswerMode(AnswerMode.SEARCH);
+        return settings;
     }
 }
